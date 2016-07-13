f7a0301d82f948753a7393dfda6a31db2908681c
okmttdhr <10101114fin@gmail.com>
2016-07-13 22:50:22 +0900

    fix lint

--- src/counter/index.js
+++ src/counter/index.js
@@ -1,28 +1,22 @@
-import ft from 'tcomb-form-types'
-
-import { t, Domain } from 'reactuate';
+import ft from 'tcomb-form-types';
+import { t, Domain, createAction, createReducer } from 'reactuate';
 
 const domain = new Domain('counter');
 export default domain;
 
-const State = t.struct({
+const state = t.struct({
   counter: ft.Number.Integer
-}, 'State')
+}, 'State');
 
-import { createAction } from 'reactuate'
+const incrementParameter = t.struct({ increment: ft.Number.Integer }, 'incrementParameter');
+const incrementCounter = createAction(domain, 'incrementCounter', t.maybe(incrementParameter));
 
-const incrementParameter = t.struct({increment: ft.Number.Integer}, 'incrementParameter')
-const incrementCounter = createAction(domain, 'incrementCounter',
-                                      t.maybe(incrementParameter))
-import { createReducer } from 'reactuate'
+const initialState = state({ counter: 0 }, 'CounterState');
 
-const initialState = State({counter: 0}, 'CounterState')
-
-const reducer = createReducer(domain, initialState,
-    incrementCounter, (state, action) => {
-      let increment = 1;
-      if (incrementParameter.is(action.payload)) {
-        increment = action.payload.increment
-      }
-      return State.update(state, {counter: { $set: state.counter + increment }})
-    })
+const reducer = createReducer(domain, initialState, incrementCounter, (_state, action) => {
+  let increment = 1;
+  if (incrementParameter.is(action.payload)) {
+    increment = action.payload.increment;
+  }
+  return state.update(_state, { counter: { $set: _state.counter + increment } });
+});
