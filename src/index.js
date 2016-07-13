3c7814e37c0b9755bd6aa3584f66c6b17fd2ebb8
okmttdhr <10101114fin@gmail.com>
2016-07-13 18:13:46 +0900

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { React, Route, Application,
-         connect, bindActionCreators } from 'reactuate'
+         connect, bindActionCreators } from 'reactuate';
 
-import counter from './counter'
-import counterAsync from './counter/async'
+import counter from './counter';
+import counterAsync from './counter/async';
 
 class App extends React.Component {
   render() {
-    return <div>{this.props.children}</div>
+    return <div>{this.props.children}</div>;
   }
 }
 
@@ -60,7 +60,7 @@ class HomePage extends React.Component {
 
 HomePage = connect(state => ({counter: state.counter.counter}),
                    dispatch => ({actions:
-                     bindActionCreators({...counter.actions, ...counterAsync.actions}, dispatch)}))(HomePage)
+                     bindActionCreators({...counter.actions, ...counterAsync.actions}, dispatch)}))(HomePage);
 
 const routes = (
   <Route component={App}>
